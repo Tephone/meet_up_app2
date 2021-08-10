6bf7c94673159249c30148ba2f76f52e26132bd0
naoko kasahara <kassa0918@gmail.com>
2021-08-11 02:11:49 +0900

    admin, student, teacherのcotrollerとviewを生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   devise_for :admins, controllers: {sessions: 'admins/sessions'}
-  devise_for :teachers, controller: {sessions: 'teachers/sessions'}
-  devise_for :students, controller: {sessions: 'students/sessions', registrations: 'students/registrations'}
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :teachers, controllers: {sessions: 'teachers/sessions'}
+  devise_for :students, controllers: {sessions: 'students/sessions', registrations: 'students/registrations'}
+  root 'tops#show'
+  resource :top, only: %i[show]
+  resource :admin, only: %i[show]
+  resources :students, only: %i[index show] do
+    collection do
+      get :home
+    end
+  end
+  resources :teachers do
+    collection do
+      get :home
+    end
+  end
 end
