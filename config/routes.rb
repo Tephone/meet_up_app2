29ee38884251b6a167eca9e8f3c5629aa77ccda4
naoko kasahara <kassa0918@gmail.com>
2021-08-11 00:15:23 +0900

    deviseでstudent, teacher, adminのmodel, controller, view作成, 日本語化, haml導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {sessions: 'admins/sessions'}
+  devise_for :teachers, controller: {sessions: 'teachers/sessions'}
+  devise_for :students, controller: {sessions: 'students/sessions', registrations: 'students/registrations'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
